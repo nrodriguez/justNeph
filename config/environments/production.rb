fdaecc52c28dfe7e44b2dcfda0690bfbbe8bf163
justNeph <nukz45@gmail.com>
2013-01-21 13:29:35 -0500

    Fix at precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ JustNeph::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( modernizr.min.js )
+  config.assets.precompile += %w( modernizr.min.js ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
