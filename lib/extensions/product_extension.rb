6a48de86946c40de79a435aa568165d7050f850f
Nephtali Rodriguez <nukz45@gmail.com>
2013-05-12 01:04:50 -0400

    Added fix to add all items from merchants

--- lib/extensions/product_extension.rb
+++ lib/extensions/product_extension.rb
@@ -1,22 +1,25 @@
 module Extensions
-  module ProductExtension
-    include "Extensions::MerchantExtensions::CafepressExtension".constantize
-    # def add_included_extensions
-    #   extensions = Pathname.glob("lib/extensions/merchant/*.rb")
-    #     .map(&:basename)
-    #     .collect{|x| 
-    #       x.to_s.gsub(".rb", "")
-    #       .titleize.gsub(" ","")
-    #     }
+  module ProductExtension    
+    def add_included_extensions
+      extensions = Pathname.glob("lib/extensions/merchant_extensions/*.rb")
+        .map(&:basename)
+        .collect{|x| 
+          x.to_s.gsub(".rb", "")
+          .titleize.gsub(" ","")
+        }
 
-    #   extensions.each do |merchant|
-    #     binding.pry
-    #     include "Extensions::MerchantExtensions::#{merchant}".constantize
-    #   end
-    # end
+      extensions.each do |merchant|
+        self.class.send :include, "Extensions::MerchantExtensions::#{merchant}".constantize
+      end
+    end
     def add_items
-      Merchant.all.each do |merchant|
-        send("add_#{merchant.name.downcase}_items")
+      begin
+        add_included_extensions
+        Merchant.all.each do |merchant|
+            send("add_#{merchant.name.downcase}_items")
+        end
+      rescue NoMethodError => e
+        "Please address: #{e}"
       end
     end
   end
