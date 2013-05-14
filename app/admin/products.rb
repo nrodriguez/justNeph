b195159479a8b935a5bc67880b0725c5f59890ec
Nephtali Rodriguez <nukz45@gmail.com>
2013-05-14 14:14:01 -0400

    Added some view changes for admin products

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,29 @@
 ActiveAdmin.register Product do
-  
+   index do
+    column :id
+    column :name
+    column :category
+    column :active
+    column :price
+    column :image do |product|
+      image_tag(product.image, height: 150, width: 150)
+    end
+    actions
+  end
+
+  show do |ad|
+    attributes_table do
+      row :name
+      row :description
+      row :category
+      row :price
+      row :active
+      row :url do
+        link_to ad.url, :target => "_blank"
+      end
+      row :image do
+        image_tag(ad.image, height: 300, width: 300)
+      end
+    end
+  end
 end
